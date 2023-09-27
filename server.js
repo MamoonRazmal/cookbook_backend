39ac83505709f0b223c5149597d29d077b94a25f
MamoonRashidRazmal <mamoonrashidrazmal@mamoonrzmalsair.localdomain>
2023-09-27 16:14:13 +0200

    connecting with frontend checked 1st-phase

--- server.js
+++ server.js
@@ -7,25 +7,31 @@ require("dotenv").config();
 const cors=require('cors')
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(logger('dev'));
 app.get("/",(req,res)=>{
 res.send("this is from api")
 })
 app.get("/recepe",(req,res)=>{
     pool.query("select * from recepe").then(data=> res.json(data.rows)).catch(e=>res.sendStatus(500).send(e))
 })
-app.post("/recepe",(req,res)=>{
-    const { food_name,food_ingredients,food_instruction,food_photo}=req.body;
-    pool.query("insert into recepe(food_name,food_ingredients,food_instruction,food_photo) values($1,$2,$3,$4) returning *",[food_name,food_ingredients,food_instruction,food_photo]).then((data)=>res.json(data.rows)).catch((e)=>res.send(500).send(e));
+app.post("/recepe",(req,res,next)=>{
+    console.log("this is from recep",req.body)
+    const {food_name,food_ingredients,food_instruction,food_photo}=req.body;
+    console.log("this the value of food_name",food_name)
+    console.log("this the value of food_ingredients",food_ingredients.toString());
+    console.log("this the value of with jSON food_ingredients",food_ingredients);
+    pool.query("insert into recepe(food_name,food_ingredients,food_instruction,food_photo) values($1,$2,$3,$4) returning *",[food_name,food_ingredients.toString(),food_instruction.toString(),food_photo]).then((data)=>res.json(data.rows)).catch((e)=>res.sendStatus(500))//.send(e));
 })
 app.delete('/recepe/:id',(req,res)=>{
     const {id}=req.params;
-    pool.query("delete from recepe where id=$1",[id]).then((data)=>res.json(data.rows)).catch((e)=>res.sendStatus(500).send(e))
+    pool.query("delete from recepe where id=$1",[id]).then((data)=>res.send(data.rows)).catch((e)=>res.sendStatus(500).send(e))
 })
 
 app.put("/recepe/:id",(req,res)=>{
     const {id}=req.params
     const { food_name,food_ingredients,food_instruction,food_photo}=req.body;
-    pool.query("update recepe set food_name=$1,food_ingredients=$2,food_instruction=$3,food_photo=$4 where id=$5 returning *",[food_name,food_ingredients,food_instruction,food_photo,id]).then((data)=>res.json(data.rows)).catch((e)=>res.sendStatus(500).send(e))
+    pool.query("update recepe set food_name=$1,food_ingredients=$2,food_instruction=$3,food_photo=$4 where id=$5 returning *",[food_name,food_ingredients,food_instruction,food_photo,id]).then((data)=>res.json(data.rows)).catch((e)=>res.sendStatus(500))
 })
     app.listen(port,()=>{
     console.log(`you are listing from http://localhost:${port}`)
